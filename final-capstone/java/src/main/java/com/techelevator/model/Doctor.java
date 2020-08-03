13b768b9910260bcead8ff87f8073a3746f25361
Adam Brennan <adamobrennan@gmail.com>
2020-08-03 14:18:39 -0400

    pushing ERD rd updates

---
+++ final-capstone/java/src/main/java/com/techelevator/model/Doctor.java
@@ -0,0 +1,14 @@
+package com.techelevator.model;
+
+public class Doctor extends User {
+
+	public Doctor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Doctor(Long id, String username, String password, String authorities) {
+		super(id, username, password, authorities);
+		// TODO Auto-generated constructor stub
+	}
+
+}
