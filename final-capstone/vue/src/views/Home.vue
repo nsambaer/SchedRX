f3067b114f140e30eda4c1c2b8708a7dcdbfc906
Dennis Bembenek <dbembenek766@gmail.com>
2020-08-05 11:31:42 -0400

    Updated Office List Component

--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -3,13 +3,15 @@
     <h1>Home</h1>
     <router-link :to="{name:'register'}">register</router-link>
 <login></login>
+<office-list></office-list>
   </div>
 </template> 
 
 <script>
 import login from '@/components/Login';
+import officeList from '@/components/OfficeList';
 export default {
   name: "home",
-  components: {login}
+  components: {login, officeList},
 };
 </script>
