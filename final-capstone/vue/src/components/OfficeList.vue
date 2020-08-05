badcc97a3808d13f1e032eb7f2c6ea070c69319f
Nicholas Sambaer <nsambaer29@gmail.com>
2020-08-05 12:03:54 -0400

    added book details tag and show logic

--- final-capstone/vue/src/components/OfficeList.vue
+++ final-capstone/vue/src/components/OfficeList.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    <div v-for="office in offices" v-bind:key="office.officeId">{{office.officeName}}</div>
+    <div v-for="office in offices" v-bind:key="office.officeId" v-toggle="office.showDetails" >
+      {{office.officeName}}
+      <div class="details" v-show="office.showDetails">
+        <office-details v-bind:office="office" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import medService from "../services/MedicalService.js";
+import OfficeDetails from '@/components/OfficeDetails';
+
 export default {
+  name: 'office-list',
+
+  components: {
+    OfficeDetails
+  },
+
   data() {
     return {
       offices: [],
@@ -15,6 +28,9 @@ export default {
   created() {
     medService.listAllOffices().then((response) => {
       this.offices = response.data;
+      this.offices.forEach( (office) => {
+        office.showDetails = false;
+      });
     });
   },
 };
