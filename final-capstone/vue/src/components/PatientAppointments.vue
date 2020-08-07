f7019a922283babcc2a61720d6370f2d15ae7d73
Dennis Bembenek <dbembenek766@gmail.com>
2020-08-07 09:31:03 -0400

    Added Patient Vue and Patient Appointment and Patient Services

---
+++ final-capstone/vue/src/components/PatientAppointments.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class = "appointment-container">
+    <div v-for="appointment in appointments" v-bind:key = "appointment.id">
+        <h2>Appointment Date: {{appointment.appointmentDate}}</h2>
+        <h3>{{appointment.appointmentTime}}</h3>
+        <h4> {{appointment.patientId}}</h4>
+        <p>{{appointment.appointmentType}}</p>
+        <p>{{appointment.visitReason}}</p>
+    </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "patient-appointments",
+
+    data(){
+        return{
+
+        }
+    },
+    props:['appointments']
+
+}
+
+</script>
+
+<style>
+
+</style>
--- final-capstone/vue/src/views/Patient.vue
+++ final-capstone/vue/src/views/Patient.vue
@@ -1,15 +1,70 @@
 <template>
-  <div>
-    <h1>This is the Patient view!</h1>
+<div class = "patient-view-container">
+    <div class = "patient-upcoming-appointments">
+      <h1> Patient upcoming appointments component will be </h1>
+    
+    <patient-appointments v-bind:appointments="appointments" />
+    </div>
+    <div class="patient-notifications">
+      <h1> Patient notifications including updated appointments and prescriptions </h1>
+    </div>
+    <div class="placeholder">
+    </div>
   </div>
+  
 </template>
 
 <script>
+
+import patientService from "../services/PatientService.js";
+import PatientAppointments from "../components/PatientAppointments.vue"
 export default {
 
+name:"patient",
+  components:{
+    PatientAppointments
+  },
+  data(){
+    return{
+      appointments:[],
+    };
+    
+  },
+  created(){
+    patientService.getAppointments(this.$store.state.user.id).then(
+      response => {
+        if(response.status == 200){
+          this.appointments = response.data;
+        }
+      }
+    );
+}
 }
 </script>
 
 <style>
+
+.patient-view-container{
+  display:grid;
+  grid-template-columns: 2fr 1fr ;
+  grid-template-areas: 
+  "notifications notifications"
+  "schedule schedule"
+  "schedule schedule";
+}
+
+.patient-upcoming-appointments{
+grid-area:"schedule";
+background-color: rgb(151, 161, 161);
+height: 400px;
+}
+
+
+.patient-notifications{
+grid-area:"notifications";
+background-color: rgb(151, 161, 161);
+}
+
+
 
 </style>
