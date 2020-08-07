14386bd1abfbc1429de72b2c018bc6b00fc63991
Peter Miller <miller.peter.james@gmail.com>
2020-08-07 12:14:11 -0400

    avaialability rendering

--- final-capstone/vue/src/components/DoctorAvailability.vue
+++ final-capstone/vue/src/components/DoctorAvailability.vue
@@ -1,15 +1,18 @@
 <template>
   <div class = "doctor-availability-container">
     <ul>
-    
-      <li v-for="(times, date) in availability.availability" v-bind:key = "date" >
-          <button v-on:click="showTimes(date)" >{{date}} </button>
+      <div v-for="(times, date) in availability.availability" v-bind:key = "date">
+      <li  v-if="times != null">
+          <button v-on:click="showDetails(date)" >{{date}} </button>
         <ul>
-          <li v-for="time in times" v-bind:key="time" v-show="date.show">
-            {{time}}
+          <div class="details" :class="date == active ? activeClass : 'hidden'">
+           <li v-for="time in times" v-bind:key="time" >
+            <button class="time-button" v-on:click="bookTime(time,date)">{{time}}</button>
           </li>
+          </div>
         </ul>
       </li>
+      </div>
   
     </ul>
 </div>
@@ -18,17 +21,36 @@
 <script>
 export default {
   name: "doctor-availability",
+   props:['availability'],
   data(){
     return{
-      
+      activeClass: 'is-visible',
+      active: null
     }
   },
+    methods:{
+    showDetails(date){
+      this.active = date;
+    
+    },
 
-  props:['availability']
+    bookTime(time,date){
+      window.alert(`you booked an appt at ${time} on ${date}`)
+    }
+  }
+ 
+
+  
 
 }
 </script>
 
 <style>
+.is-visible{
+  display:show;
+}
 
+.hidden{
+  display: none;
+}
 </style>
