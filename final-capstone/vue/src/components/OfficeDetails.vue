f61214a8ac34e937af2a7a05abca9b9dd44a16c0
Nicholas Sambaer <nsambaer29@gmail.com>
2020-08-12 17:36:00 -0400

    office details layout

--- final-capstone/vue/src/components/OfficeDetails.vue
+++ final-capstone/vue/src/components/OfficeDetails.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="office-details-container">
-    <div class="office-details" v-for="office in offices" v-bind:key="office.officeId">
+    <ul id="office-list">
+      <li v-for="office in offices" v-bind:key="office.officeId">
+        <div class="office-details">
+          <h2 class="office-name">{{ office.officeName }}</h2>
+          <p class="office-address">{{ office.address }} {{ office.city }}, {{ office.state }}</p>
+          <p class="office-phone">{{ office.phoneNumber }}</p>
+          <p class="office-cost">Cost/Hour: ${{ office.cost }}</p>
+          <table class="office-hours">
+            <td class="office-hours-days">
+              <tr>Monday</tr>
+              <tr>Tuesday</tr>
+              <tr>Wednesday</tr>
+              <tr>Thursday</tr>
+              <tr>Friday</tr>
+              <tr>Saturday</tr>
+              <tr>Sunday</tr>
+            </td>
+            <td>
+              <tr>{{ hours(office, 'Monday') }}</tr>
+              <tr>{{ hours(office, 'Tuesday') }}</tr>
+              <tr>{{ hours(office, 'Wednesday') }}</tr>
+              <tr>{{ hours(office, 'Thursday') }}</tr>
+              <tr>{{ hours(office, 'Friday') }}</tr>
+              <tr>{{ hours(office, 'Saturday') }}</tr>
+              <tr>{{ hours(office, 'Sunday') }}</tr>
+            </td>
+          </table>
+          <ul class="doctor-list">
+            <li v-for="doctor in office.doctorList" v-bind:key="doctor.doctorId">
+              {{ doctor.firstName }} {{ doctor.lastName }}
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+
+    <!--
       <table>
         <thead>
           <th>Name</th>
@@ -38,12 +74,12 @@
           </tr>
         </tbody>
       </table>
-    </div>
+    -->
   </div>
 </template>
 
 <script>
-import medService from '@/services/MedicalService';
+import medService from "@/services/MedicalService";
 
 export default {
   name: "office-details",
@@ -58,36 +94,37 @@ export default {
   methods: {
     hours(office, day) {
       if (office.openHours[day] === null) {
-        return 'Closed';
+        return "Closed";
       }
 
       let open = parseInt(office.openHours[day].substr(0, 2));
-      let close = parseInt(office.closeHours[day].substr(0,2));
-      let openFiltered = '';
+      let close = parseInt(office.closeHours[day].substr(0, 2));
+      let openFiltered = "";
       if (open < 13) {
-        openFiltered = open + ' AM';
+        openFiltered = open + " AM";
       } else {
-        openFiltered = (open % 12) + ' PM';
+        openFiltered = (open % 12) + " PM";
       }
 
-      let closeFiltered = '';
-      
+      let closeFiltered = "";
+
       if (close < 13) {
-        closeFiltered = close + ' AM';
+        closeFiltered = close + " AM";
       } else {
-        closeFiltered = (close % 12) + ' PM';
+        closeFiltered = (close % 12) + " PM";
       }
-            
-      return `${openFiltered} - ${closeFiltered}`
-    }
 
-
+      return `${openFiltered} - ${closeFiltered}`;
+    },
   },
 
   created() {
-    medService.listAllOffices().then((response) => {
-      this.offices = response.data;
-    }).catch((error) => {
+    medService
+      .listAllOffices()
+      .then((response) => {
+        this.offices = response.data;
+      })
+      .catch((error) => {
         const response = error.response;
         this.errors = true;
         if (response.status === 400) {
@@ -96,15 +133,74 @@ export default {
       });
   },
 
-  computed: {
-    
-  },
+  computed: {},
 };
 </script>
 
 
   
 <style>
+
+.office-details {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: 
+  "name name"
+  "address hours"
+  "phone hours"
+  "cost hours"
+  "doctor hours"
+  ;
+  max-width: 450px;
+}
+
+.office-name {
+  display: flex;
+  justify-content: center;
+  grid-area: name;
+}
+
+.office-address {
+  margin: 5px 0px 5px 0px;
+  grid-area: address;
+}
+
+.office-phone {
+  margin: 5px 0px 5px 0px;
+  grid-area: phone;
+}
+
+.office-cost {
+  margin: 5px 0px 5px 0px;
+  grid-area: cost;
+}
+.office-hours {
+  grid-area: hours;
+}
+
+.doctor-list {
+  margin: 5px 0px 5px 0px;
+  padding: 0px;
+  grid-area: doctor;
+}
+
+.office-hours-days {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#office-list {
+  padding: 0px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+}
+
+ul {
+  list-style: none;
+}
+
 td {
   min-width: 100px;
   text-align: center;
